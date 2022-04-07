b176df8d034517cca0b63d741d56d10cd3d0e642
prosper-20 <edwardprosper001@gmail.com>
2022-04-07 19:13:49 +0100

    updates

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, PostDetailView, PostCreateView
+from .views import Home, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path("", Home.as_view(), name="blog_home"),
     path("<int:pk>/", PostDetailView.as_view(), name='post-detail'),
-    path("create/", PostCreateView.as_view(), name="post_create")
+    path("create/", PostCreateView.as_view(), name="post_create"),
+    path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 class Home(ListView):
     model = Post
@@ -23,4 +27,19 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
