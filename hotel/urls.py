326684e69a38d553da2be7ddf4296cb11681be85
prosper-20 <edwardprosper001@gmail.com>
2022-04-07 20:02:01 +0100

    updates

--- hotel/urls.py
+++ hotel/urls.py
@@ -23,6 +23,8 @@ from blog.views import Home
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
+from .views import RoomListView, BookingListView, RoomDetailView, CancelBookingView/
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,9 +33,14 @@ urlpatterns = [
     path("register/", user_views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
-    path("profile/", user_views.profile, name="profile")
-    
+    path("profile/", user_views.profile, name="profile"),
+    # From other hotel Project
+    path("room-list/", RoomListView, name='RoomListView'),
+    path("booking-list/", BookingListView.as_view(), name="BookingListView"),
+    path("room/<category>", RoomDetailView.as_view(), name='RoomDetailView'),
+    path('booking/cancel/<pk>/', CancelBookingView.as_view(), name="CancelBookingView")
 ]
+    
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
