426691bd6eca32546a1230e5bdbaefbc6b9da157
prosper-20 <edwardprosper001@gmail.com>
2022-04-08 00:46:01 +0100

    updates

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+# from audioop import reverse
+from urllib import response
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class HomePageTests(TestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self): # new
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'app/home.html')
+
+
+
+
+
