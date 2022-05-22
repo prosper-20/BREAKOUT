0065aeb7cc12069f2018ffbd25da272304d20bf9
prosper-20 <edwardprosper001@gmail.com>
2022-05-22 23:48:36 +0100

    updates

--- hotel/urls.py
+++ hotel/urls.py
@@ -43,7 +43,8 @@ urlpatterns = [
 
     # FOR PASSWORD RESET
     path("password-reset/", auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"),
-    path("password-reset/confirm/", auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("password-reset-done/", auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
 ]
     
 
